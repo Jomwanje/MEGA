82e840340f79041c494f801322f988230f522ee6
Jomwanje <77881089+Jomwanje@users.noreply.github.com>
2021-08-09 13:51:05 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,115 @@
+<?php 
+include "connect.php";
+?>
+<!DOCTYPE HTML>
+<html class="no-js">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>MEGA Church</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<!-- Mobile Specific Metas
+  ================================================== -->
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<!-- CSS
+  ================================================== -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="plugins/mediaelement/mediaelementplayer.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="plugins/prettyphoto/css/prettyPhoto.css" rel="stylesheet" type="text/css">
+<!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="css/ie8.css" media="screen" /><![endif]-->
+<!-- Color Style -->
+<link class="alt" href="colors/color1.css" rel="stylesheet" type="text/css">
+<link href="style-switcher/css/style-switcher.css" rel="stylesheet" type="text/css">
+<!-- SCRIPTS
+  ================================================== -->
+<script src="js/modernizr.js"></script><!-- Modernizr -->
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=403202216515066";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+</head>
+<body>
+<!--[if lt IE 7]>
+	<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+<![endif]-->
+<div class="body"> 
+  <!-- Start Site Header -->
+  <header class="site-header">
+    <div class="topbar">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-8">
+            <h1 class="logo"> <a href="index.php"><img src="images/logomain.png" width="100px" alt="Logo"></a> </h1>
+          </div>
+          <div class="col-md-8 col-sm-6 col-xs-4">
+            <ul class="top-navigation ">
+              <li><a href="donate.php">Donate Now</a></li>
+            </ul>
+            <a href="#" class="visible-sm visible-xs menu-toggle"><i class="fa fa-bars"></i></a> </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-menu-wrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <nav class="navigation">
+              <ul class="sf-menu">
+                <li><a href="index.php">Home</a>
+                 </li>
+                <li><a href="about.php">About Us </a>
+				<ul class="dropdown">
+                    <li><a href="file:///C:/xampp/mysql/XAMP/htdocs/native/images/event-img3%20(2).html">           </a></li>
+                    <li><a href="file:///C:/xampp/mysql/XAMP/htdocs/native/images/event-img3%20(2).html">Slide Show Galleries</a></li>
+                    <li><a href="our-ministers.php">Church Ministers</a></li>
+                    <li><a href="our-youth.php">MEGA YOUTHS</a></li>
+                    <li><a href="our-women.php">MEGA WOMEN</a></li>
+                    <li><a href="our-men.php">MEGA MEN</a></li>
+                    <li><a href="our-children.php">MEGA CHILDREN</a></li>
+                    <li><a href="our-branches.php">CHURCH BRANCHES</a></li>
+                    <li><a href="our-faq.php">FAQ</a></li>
+                    
+                  </ul>
+                </li>
+				         <li><a href="news-updates.php">News Update</a>                
+                </li>
+
+                 <li><a href="events.php">Events</a>
+                  
+                </li>
+                <li><a href="programmes.php">Programs</a>
+                <ul class="dropdown">
+                   <li><a href="https://www.facebook.com/messianicgospelassembly">LIVE SERMONS</a></li>
+                    <li><a href="file:///C:/xampp/mysql/XAMP/htdocs/native/More/MEGA/Messianicgospelassembly.org/Webpages/Baptism.html">Baptism</a>
+                    <ul class="dropdown">
+                    <li><a href="file:///C:/xampp/mysql/XAMP/htdocs/native/More/MEGA/Messianicgospelassembly.org/Webpages/Baptism.html">Immersion or Springling?</a></li>
+                    <li><a href="our-excos.php">By Which Name Should we be Baptised?</a></li>
+                    <li><a href="our-excos.php">Who Should be Baptised</a></li>
+                    <li><a href="our-excos.php">What Happens After Baptism?</a></li>
+                  </ul>
+                  </li>
+                    <li><a href="our-excos.php">Messiah as the way</a></li>
+                    <li><a href="our-excos.php">The Ten Commandments</a></li>
+                    <li><a href="our-excos.php">The Mosaic Law</a></li>
+                    <li><a href="our-excos.php">Love</a></li>
+                    <li><a href="our-excos.php">Marriage</a></li>
+                    <li><a href="our-excos.php">Spiritual Environment</a></li>
+                    <li><a href="our-excos.php">Gurding our hearts</a></li>
+                    <li><a href="our-excos.php">The New Priesthood</a></li>
+                  </ul>
+                </li>
+                <li><a href="gallery.php">Gallery</a>          
+                </li>
+                <li><a href="contact.php">Contact Us</a> </li>
+				</ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
